00bbd47c6b6804e28e5b391fa94676b6b9d8904f
Victor Anthony <anthonyvictor90@gmail.com>
2021-09-15 15:08:01 -0300

    Fundamentals

---
+++ Exercises-JS/Fundamentals-JS/objects.js
@@ -0,0 +1,24 @@
+const aluno = {}
+
+aluno.nome = 'Victor'
+aluno.curso = 'Engenharia de Software'
+aluno.nascimento = '17/09/2002'
+
+console.log(aluno)
+
+const universidade = {
+
+    nome:'Universidade Federal do Ceara',
+    cidade:'Quixadá',
+    cursos: {
+
+        curso1:'Engenharia de software',
+        curso2:'Design Digital'
+
+    }
+
+}
+
+console.log(universidade)
+console.log(universidade.cursos)
+console.log(universidade.cursos.curso1)
