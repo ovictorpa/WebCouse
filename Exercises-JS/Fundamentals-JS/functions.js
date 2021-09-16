e239e1f44ec7b43cedb4acd11603093cb81202d1
Victor Anthony <anthonyvictor90@gmail.com>
2021-09-16 08:19:55 -0300

    Fundamentals

--- Exercises-JS/Fundamentals-JS/functions.js
+++ Exercises-JS/Fundamentals-JS/functions.js
@@ -1,14 +1,30 @@
 function impresSoma(n1, n2){
 
     console.log(n1+n2)
-
 }
-
 impresSoma(10, 10)
+
+
 
 function maisSoma(n1, n2){
 
     return n1+n2;
 }
-
 console.log(maisSoma(50,50))
+
+
+
+const outraSoma = function (n1, n2){
+
+    console.log(n1+n2)
+}
+outraSoma(50, 10)
+
+
+
+const soma = (n1, n2) => {
+
+    return n1 + n2
+}
+console.log(soma(1, 1))
+
