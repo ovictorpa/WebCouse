81ece990d81dbed8dcae25e47e99d9feac9df7a9
Victor Anthony <anthonyvictor90@gmail.com>
2021-09-28 11:57:40 -0300

    Update

---
+++ Node/modulos-terceiros.js
@@ -0,0 +1,7 @@
+const _ = require('lodash')
+
+setInterval(() => console.log(_.random(1, 1000)), 2000) // 2000 milisegundos
+
+//Numero aleatório entre 1 e 1000 em 2 segundos
+
+//nodemon modulos.terceiros.js -> executa e permite a modificação ao mesmo tempo em que executa
